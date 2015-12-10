2a9a1c4704c8ae354f5691fb867377412c907a48
Kevin321an <kevin321an@gmail.com>
2015-12-10 13:08:48 -0500

    Clean code

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ var ImgSource = [
 ];
 
 $(document).ready(function() {
-	$("#card").flip();
+	//$("#card").flip();
 //polulate the pics
 for (var i = 0; i < 2; i++) {
-	//append each pic into a row
+	//append each pic into a row seuentially
 	$.each(ImgSource, function(key, val) { 
 		var str = "<div id=card" + i + key + "><img src=" + val + " />"; 
 		$(Source).append(str);
@@ -36,45 +36,37 @@ function FlipCard() {
 	if ($("#" + id + " img").is(":hidden")) { //currently without  the card opened 
 		$(Source + " div").unbind("click", FlipCard);
 		//animation
-		$("#" + id + " img").slideDown('fast');
+		$("#" + id + " img").slideDown('slow');
 
 		//without a card has been opened 
 		if (ImgOpened == "") {
 			BoxOpened = id;
 
-			ImgOpened = $("#" + id + " img").attr("src");
-			setTimeout(function() {
-				$(Source + " div").bind("click", FlipCard)
-			}, 300);
+			ImgOpened = $("#" + id + " img").attr("src");			
+				$(Source + " div").bind("click", FlipCard);			
 		} else { 
-
 			//with card opened 
 			CurrentOpened = $("#" + id + " img").attr("src");
 			// cards no pair
-			if (ImgOpened != CurrentOpened) {
-				setTimeout(function() {
-					$("#" + id + " img").slideUp('fast');
-					$("#" + BoxOpened + " img").slideUp('fast');
+			if (ImgOpened != CurrentOpened) {				
+					$("#" + id + " img").slideUp('slow');
+					$("#" + BoxOpened + " img").slideUp('slow');
 					BoxOpened = "";
-					ImgOpened = "";
-				}, 400);
+					ImgOpened = "";				
 				//cards pair
-			} else {
-				$("#" + id + " img").parent().css("visibility", "hidden");
-				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");				
+			} else {							
 				BoxOpened = "";
 				ImgOpened = "";
-			}
-			setTimeout(function() {
-				$(Source + " div").bind("click", FlipCard)
-			}, 400);
+			}			
+				$(Source + " div").bind("click", FlipCard);
+			
 		}		
 	}
 }
-
 //shuffle the pics in random palces 
 function ShuffleImages() {
 	var ImgAll = $(Source).children();
+	//[div#card00, prevObject: n.fn.init[1], context: document, selector: "#container div:first-child"]
 	var ImgThis = $(Source + " div:first-child");
 	var ImgArr = new Array();
 
@@ -82,17 +74,13 @@ function ShuffleImages() {
 		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
 		ImgThis = ImgThis.next();
 	}	
-		ImgThis = $(Source + " div:first-child");
-	
+		ImgThis = $(Source + " div:first-child");	
 	for (var z = 0; z < ImgAll.length; z++) {
-	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+	//var RandomNumber = RandomFunction(0, ImgArr.length - 1);//0 to 10-1
+	var RandomNumber = Math.floor(Math.random()*ImgArr.length);
 
 		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
 		ImgArr.splice(RandomNumber, 1);
 		ImgThis = ImgThis.next();
 	}
 }
-function RandomFunction(MaxValue, MinValue) {
-		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
-	}
-
